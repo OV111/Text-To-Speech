fbac9219c0e868805b973d9cd3b441a12d47c883
Vahe <ohanyanv703@gmail.com>
2025-03-23 15:03:12 +0400

    play function

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const main = new SpeechSynthesisUtterance();
-
 const textInput = document.getElementById("input");
 const pauseBtn = document.getElementById("pause");
 const resumeBtn = document.getElementById("resume");
 const listenBtn = document.getElementById("listen");
+
+
+
+let voicesArray = [];
+const synth = window.speechSynthesis;
+
+
+
+function getVoice() {
+    voicesArray = synth.getVoices();
+    console.log(voicesArray);
+    let voice = voicesArray.find((x) => {
+        x.name = "Microsoft David - English (United States)";
+    });
+    if(voice) {
+        playText(text,voice);
+    }
+}
+synth.onvoiceschanged = getVoice;
+
+
+
+
+const playText = (text,voice) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    const text = textInput.value;
+    const lang = "en-US";
+    utterance.text = text;
+    utterance.lang = lang;
+    console.log(utterance);
+};
+listenBtn.addEventListener("click", playText);
+
+
+
+
+
+
 
 pauseBtn.addEventListener("click", () => {
     pauseBtn.style.display = "none";
@@ -13,23 +49,3 @@ resumeBtn.addEventListener("click", () => {
     resumeBtn.style.display = "none";
     pauseBtn.style.display = "inline";
 });
-
-
-let voicesArray = [];
-const synth = window.speechSynthesis;
-
-function getVoice( ) {
-    voicesArray = synth.getVoices();
-    console.log(voicesArray);
-}
-synth.onvoiceschanged = getVoice;
-
-const play = () => {
-    const text = textInput.value;
-    const lang = "en-US";
-    main.text = text;
-    main.lang = lang;
-    console.log(main);
-}
-
-listenBtn.addEventListener("click", play);
